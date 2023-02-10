0b7d43a2d2fa5867eb7be1b970dc93adbaca21db
Viktor St.one <pistoletviktor@gmail.com>
2023-02-10 10:39:47 +0300

    hometask1_sem4

--- task1.py
+++ task1.py
@@ -1,10 +1,16 @@
-a = int(input("введите четырех значное число "))
-counter = 0
-summa = 0
-while a > 0:
-    i = a % 10
-    a = a//10
-    counter += 1
-    print(counter)
+# a = int(input("введите четырех значное число "))
+# counter = 0
+# summa = 0
+# while a > 0:
+#     i = a % 10
+#     a = a//10
+#     counter += 1
+#     print(counter)
 
-
+dict1 = {'ad': 'hell', 'ray': 'paradise', 'gangster' : 'bandit'}
+# for item in dict1:
+#     print('--{}__{}'.format(item,dict1[item]))
+#
+# for(k,o) in dict1.items():
+#     print(k,o)
+print(dict1.items())
